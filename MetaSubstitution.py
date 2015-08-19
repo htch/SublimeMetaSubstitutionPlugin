b784dd551b0fc7e218b8ab6cb0c4bb1860a7e725
Pavel <htch.git@gmail.com>
2015-08-20 01:02:09 +0300

    Enabling override-able user settings, writing out default option+? keys

--- MetaSubstitution.py
+++ MetaSubstitution.py
@@ -7,15 +7,20 @@ class MetaSubstitutionCommand(sublime_plugin.TextCommand):
     # following https://github.com/dariusf/LambdaSubstitution/blob/master/Lambda%20Substitution.py
     def run(self, edit, key=None):
         assert(not key is None)
+        list_to_map = lambda seq: dict([(el["from"], el["to"]) for el in seq])
         if not hasattr(self, "_settings"):
-            setattr(self, "_settings", sublime.load_settings("MetaSubstitution.sublime-settings"))
+            default_settings = sublime.load_settings("MetaSubstitution.sublime-settings")
+            user_settings = sublime.load_settings("Preferences.sublime-settings")
+            meta_substitutions = list_to_map(default_settings.get("meta_substitutions", []))
+            meta_substitutions.update(list_to_map(user_settings.get("meta_substitutions", [])))
+            setattr(self, "_settings", meta_substitutions)
         selection = self.view.sel()
         stored_cursor_positions = []
         substitute_for = None
 
-        for pair in self._settings.get("meta_substitutions"):
-            if pair["from"] == key:
-                substitute_for = pair["to"]
+        for k, v in self._settings.items():
+            if k == key:
+                substitute_for = v
         
         if substitute_for is None:
             return
